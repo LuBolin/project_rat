f3684334635c5e4c9a881493f7fea687412f8ba1
Bolin <Bolin8621@gmail.com>
2025-05-21 22:29:35 +0800

    Add dummy rat

--- Source/project_rat/PlayerCharacter/PlayerCharacter.h
+++ Source/project_rat/PlayerCharacter/PlayerCharacter.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -11,19 +9,40 @@ class PROJECT_RAT_API APlayerCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+#pragma region Lifecycle
 public:
-	// Sets default values for this character's properties
 	APlayerCharacter();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+#pragma endregion
 
+
+#pragma region Input
+protected:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	void MoveForward(float Value);
+	void TurnAtRate(float Rate);
+#pragma endregion
+
+#pragma region Camera
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+#pragma endregion
+
+
+#pragma region Movement
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float SprintSpeed;
+
+private:
+	bool bIsSprinting;
+#pragma endregion
 };
