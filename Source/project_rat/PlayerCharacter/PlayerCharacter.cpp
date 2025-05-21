6c749b662a757e9ec0e90ff9b8919abbcdcfe180
Bolin <Bolin8621@gmail.com>
2025-05-22 00:11:49 +0800

    Add rat movement and camera control

--- Source/project_rat/PlayerCharacter/PlayerCharacter.cpp
+++ Source/project_rat/PlayerCharacter/PlayerCharacter.cpp
@@ -1,34 +1,76 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 
 #include "PlayerCharacter.h"
 
-// Sets default values
+
 APlayerCharacter::APlayerCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 300.0f;
+	CameraBoom->bUsePawnControlRotation = true;
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
 }
 
-// Called when the game starts or when spawned
+
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	FIA_MousePan.AddDynamic(this, &APlayerCharacter::PanCamera);
+	FIA_PlaneMove.AddDynamic(this, &APlayerCharacter::MoveInDirection);
+	FIA_Jump.AddDynamic(this, &APlayerCharacter::JumpWrapper);
 }
 
-// Called every frame
+
 void APlayerCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
-// Called to bind functionality to input
+
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 }
 
+void APlayerCharacter::PanCamera(FVector2f Input)
+{
+	const FRotator CurrentRotation = Controller->GetControlRotation();
+	float NewPitch = CurrentRotation.Pitch + Input.Y;
+	// Normalize and clamp the pitch to avoid gimbal flips
+	NewPitch = FMath::ClampAngle(NewPitch, -90.f, 0.f);
+	float NewYaw = CurrentRotation.Yaw + Input.X;
+	// Apply new rotation: keep yaw and roll
+	FRotator NewControlRot = FRotator(NewPitch, NewYaw, CurrentRotation.Roll);
+	Controller->SetControlRotation(NewControlRot);
+}
+
+
+void APlayerCharacter::MoveInDirection(FVector2f Direction)
+{
+	if (!Controller || Direction.IsNearlyZero())
+		return;
+
+	// Get yaw-only rotation (ignore pitch/roll)
+	const FRotator YawRotation(0, Controller->GetControlRotation().Yaw, 0);
+
+	// Forward and Right vectors from yaw
+	// UE world forward is X, right is Y
+	const FVector ForwardVector = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightVector = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	// Move in direction relative to camera
+	AddMovementInput(ForwardVector, Direction.Y);
+	AddMovementInput(RightVector, Direction.X); 
+}
+
+void APlayerCharacter::JumpWrapper(bool bPressed)
+{
+	// us bPressed later for varied jump height
+	// the whole cutting gravity until release thing
+	Super::Jump();
+}
--- Source/project_rat/PlayerCharacter/PlayerCharacter.h
+++ Source/project_rat/PlayerCharacter/PlayerCharacter.h
@@ -2,47 +2,94 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/StaticMeshComponent.h"
 #include "PlayerCharacter.generated.h"
+
+// input events for states to 'subscribe' to in C++
+// basically broadcast the IA delegates
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FIA_MousePan, FVector2f, Direction);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FIA_PlaneMove, FVector2f, Direction);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FIA_Jump, bool, bPressed);
+
 
 UCLASS()
 class PROJECT_RAT_API APlayerCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+#pragma region BlueprintHierarchy
+protected:
+	// From ACharacter:
+	// TObjectPtr<UCapsuleComponent> CapsuleComponent
+	// TObjectPtr<USkeletalMeshComponent> Mesh
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USpringArmComponent> CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UCameraComponent> FollowCamera;
+
+#pragma endregion
+	
+	
 #pragma region Lifecycle
 public:
 	APlayerCharacter();
+	
+	static APlayerCharacter* Instance;
 
 protected:
+	UFUNCTION()
 	virtual void BeginPlay() override;
+	
+	UFUNCTION()
 	virtual void Tick(float DeltaTime) override;
 
 #pragma endregion
 
 
 #pragma region Input
+public:
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "Input")
+	FIA_MousePan FIA_MousePan;
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "Input")
+	FIA_PlaneMove FIA_PlaneMove;
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "Input")
+	FIA_Jump FIA_Jump;
+
 protected:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	void MoveForward(float Value);
-	void TurnAtRate(float Rate);
 #pragma endregion
 
+
+	
 #pragma region Camera
 protected:
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
+	UFUNCTION()
+	void PanCamera(FVector2f Input);
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* FollowCamera;
+	UPROPERTY()
+	float MinPitch = -90.0f; // above horizontal
+	
+	UPROPERTY()
+	float MaxPitch = 0.0f; // below horizontal
+	
 #pragma endregion
 
+
+	
 
 #pragma region Movement
 protected:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
-	float SprintSpeed;
+	UFUNCTION()
+	void MoveInDirection(FVector2f Direction);
 
-private:
-	bool bIsSprinting;
+	// Jump is declared in ACharacter, and overriding forces us
+	// to use the ACharacter's UFUNCTION descriptor, which is not ideal
+	UFUNCTION()
+	void JumpWrapper(bool bPressed);
+	
 #pragma endregion
 };
